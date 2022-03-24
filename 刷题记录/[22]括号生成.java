c9ef6056976baf8ea2800cd2a1604e8d3d0f62bd
Zq-wu97 <67786452+Zq-wu97@users.noreply.github.com>
2022-03-24 09:23:49 +0800

    Add files via upload

---
+++ 刷题记录/[22]括号生成.java
@@ -0,0 +1,49 @@
+//数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：n = 3
+//输出：["((()))","(()())","(())()","()(())","()()()"]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：n = 1
+//输出：["()"]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= n <= 8 
+// 
+// Related Topics 字符串 动态规划 回溯 👍 2475 👎 0
+
+
+import java.util.ArrayList;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<>();
+        if (n == 0) return res;
+        dfs("", n, n ,res);
+        return res;
+    }
+    private void dfs(String curStr, int left, int right, List<String> res) {
+        if (left == 0 && right == 0) {
+            res.add(curStr);
+            return;
+        }
+        if (left > right) return;
+        if (left > 0) dfs(curStr + "(", left - 1, right, res);
+        if (right > 0) dfs(curStr + ")", left, right - 1, res);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
---
+++ 刷题记录/[5]最长回文子串.java
@@ -0,0 +1,60 @@
+//给你一个字符串 s，找到 s 中最长的回文子串。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：s = "babad"
+//输出："bab"
+//解释："aba" 同样是符合题意的答案。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：s = "cbbd"
+//输出："bb"
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= s.length <= 1000 
+// s 仅由数字和英文字母组成 
+// 
+// Related Topics 字符串 动态规划 👍 4907 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public String longestPalindrome(String s) {
+        int len = s.length();
+        if (len < 2) return s;
+        int maxLen = 1;
+        int begin = 0;
+        boolean[][] dp = new boolean[len][len];
+        for (int i = 0; i < len; i++) {
+            dp[i][i] = true;
+        }
+        char[] charArray = s.toCharArray();
+        for (int L = 2; L <= len; L++) {
+            for (int i = 0; i < len; i++) {
+                int j = L + i - 1;
+                if (j >= len) break;
+                if (charArray[j] != charArray[i]) dp[i][j] = false;
+                else if (j - i + 1 < 3) dp[i][j] = true;
+                else dp[i][j] = dp[i + 1][j - 1];
+                if (dp[i][j] && j - i + 1 > maxLen) {
+                    maxLen = j - i + 1;
+                    begin = i;
+            }
+
+            }
+        }
+        return s.substring(begin, begin + maxLen);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
